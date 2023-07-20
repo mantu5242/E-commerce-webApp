a0d55abb6a0f6901a012a564c59db515dbc0c227
mantu5242 <mantukr5242@gmail.com>
2023-07-20 22:35:02 +0530

    new update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,34 +1,13 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { productsAction } from '../redux/actions/products';
-import ProductItems from '../components/ProductItems';
-import { searchAction } from '../redux/actions/search';
-import Slider from '../components/Slider';
-
+import { Navbar, Main, Product, Footer } from "../components";
 
 function Home() {
-  const dispatch = useDispatch();
-  const{products} = useSelector(state=> state.products);
-  const{search} = useSelector(state=> state.search);
-  useEffect(()=>{
-    dispatch(productsAction())
-    dispatch(searchAction())
-  },[dispatch])
-  
   return (
-    <div>
-      <Slider/>
-      <div className='flex flex-wrap justify-center'>    
-      {
-      search.length > 0 ? ( 
-        search.map((val,i)=>(
-        <ProductItems key={i} val={val}/>
-      ))):
-      products && products.map((val,i)=>(
-        <ProductItems key={i} val={val}/>
-      ))}
-       </div>
-    </div>
+    <>
+      <Navbar />
+      <Main />
+      <Product />
+      <Footer />
+    </>
   )
 }
 
